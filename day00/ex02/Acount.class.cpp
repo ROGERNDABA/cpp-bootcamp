aefb32285de5ba049a67f37ca2778ea67c804368
rogerndaba <rogerndaba@gmail.com>
2019-05-26 14:44:58 +0200

    confussed printing

--- day00/ex02/Acount.class.cpp
+++ day00/ex02/Acount.class.cpp
@@ -15,11 +15,15 @@ Account::Account(int initial_deposit) {
     if (initial_deposit > 0) {
         this->_amount += initial_deposit;
         this->_totalAmount += initial_deposit;
-        this->_totalNbDeposits++;
+        // this->_totalNbDeposits++;
     }
     this->_accountIndex = this->_nbAccounts;
+    this->_displayTimestamp();
+    std::cout << " index:" << this->_accountIndex << ";";
+    std::cout << "Amount:" << this->_amount << ";";
+    std::cout << "created" << std::endl;
+    // this->displayStatus();
     this->_nbAccounts++;
-    // std::cout << "created" << std::endl;
 };
 
 void Account::makeDeposit(int deposit) {
@@ -42,10 +46,14 @@ bool Account::makeWithdrawal(int withdrawal) {
 };
 
 int Account::checkAmount(void) const {
+    std::cout << "dfdfdfdfdfdfdfd" << std::endl;
     return this->_totalAmount;
 };
 
 Account::~Account() {
+    this->_displayTimestamp();
+    std::cout << " index:" << this->_accountIndex << ";";
+    std::cout << "Amount:" << this->_amount << ";";
     std::cout << "closed" << std::endl;
 };
 
@@ -55,7 +63,7 @@ void Account::_displayTimestamp(void) {
     time_t now = time(0);
     tm *ltm = localtime(&now);
 
-    std::cout << "[" << ltm->tm_year;
+    std::cout << "[" << 1970 + ltm->tm_year;
     std::cout << 1 + ltm->tm_mon;
     std::cout << ltm->tm_mday;
     std::cout << "_" << 1 + ltm->tm_hour;
@@ -63,11 +71,13 @@ void Account::_displayTimestamp(void) {
     std::cout << 1 + ltm->tm_sec << "]";
 }
 void Account::displayAccountsInfos() {
-    _displayTimestamp();
-    std::cout << " accounts:" << _nbAccounts << ";";
-    std::cout << "total:" << _totalAmount << ";";
-    std::cout << "deposits:" << _totalNbDeposits << ";";
-    std::cout << "withdrawals:" << _totalNbWithdrawals << ";" << std::endl;
+    if (_totalNbWithdrawals) {
+        _displayTimestamp();
+        std::cout << "\033[31m accounts:" << _nbAccounts << ";";
+        std::cout << "total:" << _totalAmount << ";";
+        std::cout << "deposits:" << _totalNbDeposits << ";";
+        std::cout << "withdrawals:" << _totalNbWithdrawals << ";\033[0m" << std::endl;
+    }
 }
 
 void Account::displayStatus() const {
