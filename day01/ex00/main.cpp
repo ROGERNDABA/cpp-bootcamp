fbde970f31c87e558276351db6ec2459d8477638
Roger NDABA <rmdaba@c4r19s5.wethinkcode.co.za>
2019-05-29 16:21:26 +0200

    dg

--- day01/ex00/main.cpp
+++ day01/ex00/main.cpp
@@ -2,9 +2,11 @@
 
 int main() {
     Pony ponyStack = ponyOnTheStack();
+    //Use of a dot to access member of the Pony class if created on the stack
     ponyStack.printPony();
 
     Pony *ponyHeap = ponyOnTheHeap();
+    //Use of an arrow to access member of the Pony class if created on the heap
     ponyHeap->printPony();
     //Have to explicitly free memory with delete
     delete ponyHeap;
