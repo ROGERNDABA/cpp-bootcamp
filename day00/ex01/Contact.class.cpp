33f5d9814a0dab0c1d8df63a5b7675d3248f334d
roger NDABA <rmdaba@c4r16s8.wethinkcode.co.za>
2019-06-04 12:01:35 +0200

    fixed inclusion issues d00 ex02

--- day00/ex01/Contact.class.cpp
+++ day00/ex01/Contact.class.cpp
@@ -10,34 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Contact.h"
-
-class Contact {
-   private:
-    TContact contact;
-
-    static int contactCount;
-
-   public:
-    Contact(TContact);
-
-    std::string getFirstname() { return this->contact.firstname; };
-    std::string getLastname() { return this->contact.lastname; };
-    std::string getNickname() { return this->contact.nickname; };
-    std::string getLogin() { return this->contact.login; };
-    std::string getAddress() { return this->contact.address; };
-    std::string getEmailAddress() { return this->contact.emailAddress; };
-    std::string getPhoneNumber() { return this->contact.phoneNumber; };
-    std::string getBirthdayDate() { return this->contact.birthdayDate; };
-    std::string getFavoriteMeal() { return this->contact.favoriteMeal; };
-    std::string getUnderwear() { return this->contact.underwear; };
-    std::string getColor() { return this->contact.color; };
-    std::string getDarkestSecret() { return this->contact.darkestSecret; };
-    int getContactCount() { return this->contactCount; };
-
-    Contact(void);
-    ~Contact();
-};
+#include "Contact.hpp"
 
 int Contact::contactCount = 0;
 
@@ -46,6 +19,6 @@ Contact::Contact(TContact contact) {
     Contact::contactCount++;
 }
 
-Contact::Contact(void){};
+Contact::Contact(){};
 
 Contact::~Contact() {}
--- day00/ex01/Contact.hpp
+++ day00/ex01/Contact.hpp
@@ -31,4 +31,31 @@ typedef struct SContact {
     std::string darkestSecret;
 } TContact;
 
+class Contact {
+   private:
+    TContact contact;
+
+    static int contactCount;
+
+   public:
+    Contact(TContact);
+
+    std::string getFirstname() { return this->contact.firstname; };
+    std::string getLastname() { return this->contact.lastname; };
+    std::string getNickname() { return this->contact.nickname; };
+    std::string getLogin() { return this->contact.login; };
+    std::string getAddress() { return this->contact.address; };
+    std::string getEmailAddress() { return this->contact.emailAddress; };
+    std::string getPhoneNumber() { return this->contact.phoneNumber; };
+    std::string getBirthdayDate() { return this->contact.birthdayDate; };
+    std::string getFavoriteMeal() { return this->contact.favoriteMeal; };
+    std::string getUnderwear() { return this->contact.underwear; };
+    std::string getColor() { return this->contact.color; };
+    std::string getDarkestSecret() { return this->contact.darkestSecret; };
+    int getContactCount() { return this->contactCount; };
+
+    Contact(void);
+    ~Contact();
+};
+
 #endif  // CONTACT_H
