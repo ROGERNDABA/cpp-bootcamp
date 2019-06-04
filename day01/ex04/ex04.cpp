16f77651c3078fe85bdf2a90fbef027be4e5eb63
roger NDABA <rmdaba@c4r16s8.wethinkcode.co.za>
2019-06-04 14:40:39 +0200

    I don't understand d01 ex04

--- day01/ex04/ex04.cpp
+++ day01/ex04/ex04.cpp
@@ -3,10 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ex04.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmdaba <rmdaba@student.42.fr>              +#+  +:+       +#+        */
+/*   By: Roger Ndaba <rogerndaba@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 14:25:55 by rmdaba            #+#    #+#             */
-/*   Updated: 2019/06/04 14:25:57 by rmdaba           ###   ########.fr       */
+/*   Updated: 2019/06/04 14:39:52 by Roger Ndaba      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main() {
+    std::string theString = "HI THIS IS BRAIN";
+    std::string *ptrString = new std::string(theString);
+    // std::string refString = &ptrString;
+    std::cout << ptrString << std::endl;
+    return 0;
+}
