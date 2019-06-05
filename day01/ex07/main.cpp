92c7e14488c256d54b2e75f8d95999c3ac06048a
rogerndaba <rogerndaba@gmail.com>
2019-06-05 20:24:04 +0200

    shit can't use c++11 functons

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -6,12 +6,13 @@
 /*   By: Roger Ndaba <rogerndaba@gmil.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 19:10:07 by Roger Ndaba       #+#    #+#             */
-/*   Updated: 2019/06/05 20:13:03 by Roger Ndaba      ###   ########.fr       */
+/*   Updated: 2019/06/05 20:18:28 by Roger Ndaba      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
+#include <string>
 
 int main(int ac, char *av[]) {
     if (ac == 4) {
@@ -23,24 +24,30 @@ int main(int ac, char *av[]) {
 
         if (ifs.fail()) {
             std::cout << "Failed to open file" << std::endl;
-            exit(EXIT_FAILURE);
+            exit(1);
         }
         std::string outputFile = filename + ".replace";
         std::ofstream ofs(outputFile);
 
         std::string buff;
         do {
-            // while (buff.find(s1) != std::string::npos) {
-            //     buff.replace(buff.find(s1), s1.length(), s2);
-            // }
-            // ofs << buff;
-            // ofs << std::endl;
+            while (buff.find(s1) != std::string::npos) {
+                buff.replace(buff.find(s1), s1.length(), s2);
+            }
+            ofs << buff;
+            ofs << std::endl;
         } while (std::getline(ifs, buff));
 
-        std::cout
-            << "filename ->" << filename << std::endl;
-        std::cout << "s1 ->" << s1 << std::endl;
-        std::cout << "s2 ->" << s2 << std::endl;
+        if (buff == "") {
+            ofs << std::endl;
+        }
+        ifs.close();
+        ofs.close();
+
+        // std::cout
+        //     << "filename ->" << filename << std::endl;
+        // std::cout << "s1 ->" << s1 << std::endl;
+        // std::cout << "s2 ->" << s2 << std::endl;
 
     } else {
         std::cout << "no" << std::endl;
