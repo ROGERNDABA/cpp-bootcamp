1726a669081414eeb2af1c5323032bf680e067c4
roger NDABA <rmdaba@c4r16s10.wethinkcode.co.za>
2019-06-05 11:27:59 +0200

    mxm

--- day01/ex04/ex04.cpp
+++ day01/ex04/ex04.cpp
@@ -6,17 +6,17 @@
 /*   By: Roger Ndaba <rogerndaba@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 14:25:55 by rmdaba            #+#    #+#             */
-/*   Updated: 2019/06/04 14:39:52 by Roger Ndaba      ###   ########.fr       */
+/*   Updated: 2019/06/05 11:27:35 by Roger Ndaba      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int main() {
-    std::string theString = "HI THIS IS BRAIN";
-    std::string *ptrString = new std::string(theString);
+    std::string *theString = new std::string("HI THIS IS BRAIN");
+    std::string *ptrString = theString;
     std::string newString = *ptrString;
     delete ptrString;
-    std::cout << newString << std::endl;
+    std::cout << ptrString << std::endl;
     return 0;
 }
