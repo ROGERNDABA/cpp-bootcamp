49cec35021886ea6aa386e3551e87894b90a7f01
roger NDABA <rmdaba@c4r16s10.wethinkcode.co.za>
2019-06-06 11:58:55 +0200

    ex10 gg

--- day01/ex10/main.cpp
+++ day01/ex10/main.cpp
@@ -6,12 +6,34 @@
 /*   By: Roger Ndaba <rogerndaba@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 11:32:53 by Roger Ndaba       #+#    #+#             */
-/*   Updated: 2019/06/06 11:33:20 by Roger Ndaba      ###   ########.fr       */
+/*   Updated: 2019/06/06 11:57:01 by Roger Ndaba      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fstream>
 #include <iostream>
 
-int main(int argc, char const *argv[]) {
-        return 0;
+int main(int ac, char const *av[]) {
+    if (ac > 1) {
+        for (int i = 1; i < ac; i++) {
+            std::ifstream ifs(av[i]);
+            if (ifs.fail()) {
+                std::cout << "No such file or directory : " << av[i];
+                ifs.close();
+            }
+            std::string buff;
+            while (std::getline(ifs, buff)) {
+                std::cout << buff;
+            };
+            ifs.close();
+        }
+    } else {
+        std::string cmd;
+        while (1) {
+            std::getline(std::cin, cmd);
+            std::cout << cmd << std::endl;
+        }
+    }
+
+    return 0;
 }
