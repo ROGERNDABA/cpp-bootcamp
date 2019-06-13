e7683cec370a5999a6bcf3bfd9af33b3a3b395e7
roger NDABA <rmdaba@c4r16s10.wethinkcode.co.za>
2019-06-13 16:07:39 +0200

    xfd

--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Roger Ndaba <rogerndaba@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 15:32:33 by Roger Ndaba       #+#    #+#             */
-/*   Updated: 2019/06/13 15:55:19 by Roger Ndaba      ###   ########.fr       */
+/*   Updated: 2019/06/13 16:07:25 by Roger Ndaba      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,26 @@ struct Data {
 void* serialize(void) {
     Data* dataPtr = new Data;
     std::string alphaNumerics = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (size_t i = 0; i < 8; i++)
+    for (size_t i = 0; i < 8; i++) {
         dataPtr->s1 += alphaNumerics[rand() % 61];
+    }
     dataPtr->n = rand();
-    for (size_t i = 0; i < 8; i++)
+    for (size_t i = 0; i < 8; i++) {
         dataPtr->s2 += alphaNumerics[rand() % 61];
+    }
     return dataPtr;
-}
+};
 
 Data* deserialize(void* raw) {
     Data* res = reinterpret_cast<Data*>(raw);
     return res;
-}
+};
 
 int main() {
-    srand(time(NULL));
-    void* serialize = serialize();
-    Data* deserialize = deserialize(serialize);
+    srand(time(0));
+    void* ser = serialize();
+    Data* des = deserialize(ser);
+    std::cout << des->s1 << des->n << des->s2 << std::endl;
+    delete des;
     return 0;
 }
