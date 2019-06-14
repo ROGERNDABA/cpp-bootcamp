9ca9a6953e12090b6843d02170c18bd9f1558eca
roger NDABA <rmdaba@c4r16s10.wethinkcode.co.za>
2019-06-14 09:23:08 +0200

    d07  ex00 eeez

---
+++ day07/ex00/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Roger Ndaba <rogerndaba@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/14 09:12:27 by Roger Ndaba       #+#    #+#             */
+/*   Updated: 2019/06/14 09:22:32 by Roger Ndaba      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+template <typename T>
+
+void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T const &min(T const &a, T const &b) {
+    return ((a <= b) ? a : b);
+}
+
+template <typename T>
+T const &max(T const &a, T const &b) {
+    return ((a >= b) ? a : b);
+}
+
+int main() {
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    return 0;
+}
